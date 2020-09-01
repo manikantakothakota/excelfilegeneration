475290c21720453e91d13de4e01e10bcb5eb8546
manikanta <manikanta.kottakota322@gmail.com>
2020-09-01 15:07:51 +0530

    changes done

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
@@ -27,15 +31,20 @@ public class UserController {
 		return Userlist;
 	}
 	
-	@GetMapping(value = "/excelExp") 
-	public ModelAndView doExcelExport() {   
+	@GetMapping(value = "/excelExp",produces = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") 
+	public ResponseEntity<InputStreamResource> excelCustomersReport() throws IOException {
 		List<User> Userlist=userservice.loadAllUsers();
-		ModelAndView m=new ModelAndView();
-		UserExcelview uev=new UserExcelview();
-		m.setView(uev);
-		m.addObject("userlist", Userlist);
-		return m;
-
-		
-	} 
+    
+    ByteArrayInputStream in = UserExcelview.UsersToExcel(Userlist);
+    // return IOUtils.toByteArray(in);
+    
+    HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=Users.xlsx");
+    
+     return ResponseEntity
+                  .ok()
+                  .headers(headers)
+                  .body(new InputStreamResource(in)
+                		);
+    }
 }
--- src/main/java/com/example/demo/util/UserExcelview.java
+++ src/main/java/com/example/demo/util/UserExcelview.java
@@ -1,67 +1,77 @@
 package com.example.demo.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.springframework.web.servlet.view.document.AbstractXlsxView;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.example.demo.entity.User;
 
-public class UserExcelview extends AbstractXlsxView{
+public class UserExcelview{
 
-	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		
-		
-response.setHeader("Content-Disposition", "attachment;filename=UserDetails.xlsx");
 
-		
-		@SuppressWarnings("unchecked")
-		List<User> User=(List<User>) model.get("userlist");
-	
-		Sheet sheet=workbook.createSheet("User");
-		
-		setHead(sheet);
-		setBody(sheet,User);
-		
-	}
+	public static ByteArrayInputStream UsersToExcel(List<User> users) throws IOException {
+		String[] COLUMNs = {"ID", "NAME", "ROLE", "EMAIL","PHONE","BUSINESS","COVID19_EFFECT_ON_BUSINESS"};
+		try(
+				Workbook workbook = new XSSFWorkbook();
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				){
+			CreationHelper createHelper = workbook.getCreationHelper();
 
-	private void setHead(Sheet sheet) {
+			Sheet sheet = workbook.createSheet("Users");
 
-		Row row=sheet.createRow(0);
-		row.createCell(0).setCellValue("ID");
-		row.createCell(1).setCellValue("NAME");
-		row.createCell(2).setCellValue("ROLE");
-		row.createCell(3).setCellValue("EMAIL");
-		row.createCell(4).setCellValue("PHONE");
-		row.createCell(5).setCellValue("BUSINESS");
-		row.createCell(6).setCellValue("COVID19_EFFECT_ON_BUSINESS");
+			Font headerFont = workbook.createFont();
+			headerFont.setBold(true);
+			headerFont.setColor(IndexedColors.BLUE.getIndex());
+
+			CellStyle headerCellStyle = workbook.createCellStyle();
+			headerCellStyle.setFont(headerFont);
+
+			// Row for Header
+			Row headerRow = sheet.createRow(0);
+
+			// Header
+			for (int col = 0; col < COLUMNs.length; col++) {
+				Cell cell = headerRow.createCell(col);
+				cell.setCellValue(COLUMNs[col]);
+				cell.setCellStyle(headerCellStyle);
 			}
-	
-	private void setBody(Sheet sheet, List<User> user) {
 
-		int rowNumber=1;
-		for(User u:user) {
-			Row row=sheet.createRow(rowNumber++);
-			row.createCell(0).setCellValue(u.getId().toString());
-			row.createCell(1).setCellValue(u.getName());
-		    row.createCell(2).setCellValue(u.getRole());
-			row.createCell(3).setCellValue(u.getEmail());
-			row.createCell(4).setCellValue(u.getPhone());
-			row.createCell(5).setCellValue(u.getBusiness());
-			row.createCell(6).setCellValue(u.getCovid19EffectOnBusiness());
-			
+			// CellStyle for Age
+			CellStyle ageCellStyle = workbook.createCellStyle();
+			ageCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("#"));
+
+			int rowIdx = 1;
+			for (User user : users) {
+				Row row = sheet.createRow(rowIdx++);
+
+				row.createCell(0).setCellValue(user.getId());
+				row.createCell(1).setCellValue(user.getName());
+				row.createCell(2).setCellValue(user.getRole());
+				row.createCell(3).setCellValue(user.getEmail());
+				row.createCell(4).setCellValue(user.getPhone());
+				row.createCell(5).setCellValue(user.getBusiness());
+				row.createCell(6).setCellValue(user.getCovid19EffectOnBusiness());
+
+			}
+
+			workbook.write(out);
+			return new ByteArrayInputStream(out.toByteArray());
 		}
-		
 	}
-		
-		
-	}
+
+
+}
+
 
 
